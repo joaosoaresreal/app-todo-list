103c346021e688739796701f714090e41d553114
João Soares <83253786+joaosoaresreal@users.noreply.github.com>
2023-05-08 22:30:05 -0400

    Alterações no Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Theme } from "./theme/ThemeProvider"
 import CssBaseline from '@mui/material/CssBaseline';
-import { AppBar, Badge, Button, colors, Grid, TextField, Toolbar, Typography, useTheme } from "@mui/material";
+import { AppBar, Badge, Button, colors, Grid, TextField, Toolbar, Typography, useTheme, Divider } from "@mui/material";
 import { Container } from "@mui/system";
-import { PlusCircle, Rocket } from "phosphor-react";
+import { PlusCircle, Rocket, ClipboardText } from "phosphor-react";
 
 
 function App() {
@@ -23,15 +23,16 @@ function App() {
             }
           }}>
             <Typography variant="h5" component="h1" sx={{
-              display:"flex",
-              alignItems:"center",
-              gap:tema.spacing(2),
-              color:tema.palette.primary.light
+              display: "flex",
+              alignItems: "center",
+              gap: tema.spacing(2),
+              color: tema.palette.primary.light
             }}>
-              <Rocket size={32}/>Lista de Tarefas
+              <Rocket size={32} />Lista de Tarefas
             </Typography>
           </Toolbar>
         </AppBar>
+
         <main>
           <Container sx={{
             position: "relative",
@@ -53,21 +54,62 @@ function App() {
               </Grid>
             </Grid>
 
-            <Grid container spacing={tema.spacing(1)}>
-              <Grid item>
+            <Grid container spacing={tema.spacing(4)}>
+
+              <Grid item sx={{
+                display: "flex"
+              }}>
                 <Typography variant="caption" sx={{
-                  display:"flex",
-                  alignItems:"center",
-                  gap:tema.spacing(1)
+                  //display: "flex",
+                  alignItems: "center",
+                  gap: tema.spacing(0)
                 }}>
                   <Badge color="primary" badgeContent={0} showZero>Tarefas Criadas</Badge>
                 </Typography>
+
+                <Typography variant="caption" sx={{
+                  //display: "flex",
+                  alignItems: "center",
+                  gap: tema.spacing(1)
+                }}>
+                  <Badge color="primary" badgeContent={0} showZero>Concluídas</Badge>
+                </Typography>
+
               </Grid>
+
               {/*flex */}
+            </Grid>
+
+            <Divider /> {/* Linha que faz a divisão*/}
+
+            <Grid container spacing={tema.spacing(0)}>
+              <Grid xl={12} xs={12} sx={{
+                //display: "flex",
+                alignItems: "center",
+                textAlign: 'center'
+              }}>
+
+                <ClipboardText size={56} color={colors.grey[700]} weight="light" />
+
+                <Typography variant='h6' sx={{
+                  color: colors.grey[600],
+                  fontWeight: 'bold',
+                }}>
+                  Você ainda não tem tarefas cadastradas
+                </Typography>
+
+                <Typography variant='h6' sx={{
+                  color: colors.grey[800],
+                }}>
+                  Crie tarefas e organize seus itens a fazer
+                </Typography>
+
+              </Grid>
             </Grid>
 
           </Container>
         </main>
+
       </Theme>
     </>
   )
