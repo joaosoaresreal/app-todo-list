e8d51df4c97cabd8334020f6c58d93cc11c30c55
joaosoaresreal <joao.soares8@estudante.ifms.edu.br>
2023-05-11 20:56:46 -0400

    Configuração da API

---
+++ src/service/api.ts
@@ -0,0 +1,48 @@
+import axios from 'axios'
+import { Todos } from '../types';
+
+// Api que conecta com o banco usando 'axios'
+const api = axios.create({
+    baseURL: 'http://localhost:8081',
+    timeout: 1000 // tempo de atraso na resposta
+});
+
+// Listar todos os registro do banco
+export async function getAll() {
+    const todos = await api.get('todos')
+    return todos.data
+}
+
+// Listar registro unico do banco
+export async function getById(id:number) {
+    const todos = await api.get(`todos/${id}`)
+    return todos.data
+}
+
+// Salvar registro no banco
+export async function save(todos:Todos) {
+    const response = await api.post(`todos`,{
+        ...todos
+    })
+    return response.data
+}
+
+// Atualizar registro do banco
+export async function update(todos:Todos) {
+    const response = await api.put(`todos/${todos.id}`,{
+        ...todos
+        /*
+            É A MESMA COISA QUE FAZER ISSO:
+            
+            description:todos.description,
+            done:todos.done
+        */
+    })
+    return response.data
+}
+
+// Exluir registro do banco
+export async function exclude(id:number) {
+    const todos = await api.delete(`todos/${id}`)
+    return todos.data
+}
